bc35bd2a2b90a8d9b44a8d72eaca62b33f494409
simppasofia <sinisofiakor@gmail.com>
2024-04-28 10:40:20 +0300

    Tehtävä 1.9: unicafe step4

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -13,21 +13,31 @@ const Statistics = ({good, bad, neutral}) => {
   const avg = (good - bad) / all
   const pctPositive = good / all * 100
 
-  return (
-    <div>
-      <h1>Statistics</h1>
-      good {good}<br />
-      neutral {neutral}<br/>
-      bad {bad}<br />
-      all {all}
-
-      <p>
-        average {avg}<br />
-        positive {pctPositive} %
-      </p>
-    </div>
-  )
+  if (all == 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        No feedback given
+      </div>
+    ) 
+  } else {
+    return (
+      <div>
+          <h1>Statistics</h1>
+          good {good}<br />
+          neutral {neutral}<br/>
+          bad {bad}<br />
+          all {all}
+  
+          <p>
+            average {avg}<br />
+            positive {pctPositive} %
+          </p>
+      </div>
+    )
+  }
 }
+
 
 const App = () => {
   // tallenna napit omaan tilaansa
